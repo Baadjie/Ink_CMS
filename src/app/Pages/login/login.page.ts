9a69cbcea71499dab6d418271b5458596c601dc8
Baadjie <baloyipastor92@gmail.com>
2019-11-26 14:12:24 +0200

    changes

--- src/app/Pages/login/login.page.ts
+++ src/app/Pages/login/login.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import* as firebase from '@ionic-native/firebase';
+import { Router } from '@angular/router';
+//import * as firebase from 'firebase/app';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  db = firebase.firestor
+  username: string=""
+  password: string=""
+  constructor(
+    private router: Router
+   
+  ) {}
+
 
   ngOnInit() {
   }
+
+ 
+  Login(){
+    const {username,password}=this
+    firebase.auth().signInWithEmailAndPassword(username, password).then((result) => {
+      
+      console.log("Logged in succesful")
+      this.router.navigateByUrl('/main-page');
+  }).catch((error) => {
+    console.log("User not found")
+     let errorCode = error.code;
+     let errorMessage = error.message;
+ 
+  });
+
+  }
+
+
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:    {apiKey: "AIzaSyC3vCG0b69VGuhdnn82Ra6xLoI-3IAG-q0",
+  authDomain: "ink-tatoos.firebaseapp.com",
+  databaseURL: "https://ink-tatoos.firebaseio.com",
+  projectId: "ink-tatoos",
+  storageBucket: "ink-tatoos.appspot.com",
+  messagingSenderId: "614855155179",
+  appId: "1:614855155179:web:ea09d009668a40b4a1cdae",
+  measurementId: "G-694DP6LKJV"
+}
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
